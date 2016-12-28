97cb9e665b43685b5638123515ef787ed9b3755f
Reed Wade <reed@typist.geek.nz>
2016-12-29 01:26:55 +1300

    more typo

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 
 setup(name='pikatopic',
-      version='1.0.2',
-      description='A convenience layer atop Pika for use with RabbitMQ topic exhanges.',
+      version='1.0.3',
+      description='A convenience layer atop Pika for use with RabbitMQ topic exchanges.',
       url='http://github.com/AnimationMentor/pikatopic',
       author='Reed Wade for Artella.com',
       author_email='reed@artella.com',
